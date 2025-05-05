6c4199611e9316f53477ea0bd51aa9ffbe088b09
JPChoyon <kotjpckhan@gmail.com>
2025-05-05 22:49:53 +0600

    home page meta data added

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -3,6 +3,12 @@ import Gallery from "@/components/home/Gallery/gallery";
 import Banner from "@/components/home/header/Banner";
 import Testimonial from "@/components/home/Testimonials/testimonial";
 import { NewsletterSignup } from "@/components/newsLetter/newsLetter";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tasty Food - Home",
+  description: "Tasty Food - Home",
+};
 
 export default function Home() {
   return (
